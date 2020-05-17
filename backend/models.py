5c8084ad52e5beeeb5c555369bc64ac2bcdc837a
marissawatkins <mjw2d4@mail.missouri.edu>
2020-05-17 15:43:24 -0700

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = 'trivia'
-database_path = "postgres://{}:{}@{}/{}".format('postgres', 'Lamp,post1', 'localhost:5432', database_name)
+database_path = "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
